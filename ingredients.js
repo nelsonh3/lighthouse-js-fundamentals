56d4ad80ec94c87e608b0b80686769fcee36d484
Nelson Huang <n-huang@live.com>
2021-05-03 01:12:10 +0000

    Loop Reverse

---
+++ ingredients.js
@@ -0,0 +1,6 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for(let i = 7; i>= 0; i--){
+  console.log(ingredients[i]);
+  }
+   
